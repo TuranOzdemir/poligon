3ed1e5e687f87edc53832651316d53b19b2e764f
Turan OZDEMİR <62646952+TuranOzdemir@users.noreply.github.com>
2021-09-05 21:28:32 +0300

    coment

--- dayaliPoligonHesabi.py
+++ dayaliPoligonHesabi.py
@@ -1,61 +1,63 @@
 
 import math as m
-Sn = []
-Pn = []
-Bt = []
-yeniBt = []
-Pcount = 4
+Sn = [] # verilen iki nokta arası uzaklılıkları
+Pn = [] # sonuç listesi bu listede bulunmak istenen noktaların koordinatları ikili şekilde bulunacak
+Bt = [] # verilen açılar
+yeniBt = [] # kırılma acısı hatası hesaplanırken güncellenen Bt değerlerinin konulacağı liste 
 devam = False
-Slis = []
-
+# Bt = kırılma açıları 
+# Bt_sum = beta kırılma açıları toplamı .
+# IS = ilk semt açısı
+# SS = son semt açısı
+# devam = "devam = true" olursa eğer tekrar ölçüm yapılması gerek 
+# bu nedenle kullanıcıya yeniden ölçüm yap mesajı verilir ve hesaba devam edilmez
+# .
 
 def kirilmaAcisiHatasi(Bt, IS, SS, devam):
     Bt_Sum = sum(Bt)
     
-    fB = SS - (IS + Bt_Sum + len(Bt)+200) #Bt_Sum dan sonra gelen + veya - olacak kontrol et /// +200 grad cinsinden hata olabilir
-    FB = 1,5 * m.sqrt(len(Bt))
+    fB = SS - (IS + Bt_Sum + len(Bt)+(200*180/200)) # kırılma açılarında yapılan hata miktarı
+    FB = 1,5 * m.sqrt(len(Bt)) # yönetmelikteki formül(aşşağıdaki kontrol evresinde hata payının kabul edilebilirliğini kontrol etmek için kullanılır)
     if fB < FB:
-        x = fB/len(Bt)
+        x = fB/len(Bt) # hata miktarı bt adedine bölünerek bt lere eklemek için hazır hale getirilir
     else :
         devam == True
     for i in Bt:
-        e = i+x
-        yeniBt.append(e)
-    return yeniBt
+        e = i+x # sıradaki bt değerine daha önce bt adedine bölünerek bulunan x eklenir
+        yeniBt.append(e) # hata kabul edilebilirse; Bt leri güncellemek için yeniBt listesi oluşturulur 
+                         # ve oluşan listeye güncellenmiş bt eklenir
+    return yeniBt # böylece kenar kapatma hatası düzelir ve hesapta kullanılmak üzere bt; yeniBt olarak güncellenmiş olur
 
-def kenKapHata(Dy, Dx, Yb, Xb, Yc, Xc, Bt, Slis):
-    Dyt = sum(Dy)
-    Dxt = sum(Dx)
-    S = m.sqrt(Dyt**2 + Dxt**2)
-    Fy = Yc - Yb
-    Fx = Xc - Xb
-    fy = Fy - Dyt
-    fx = Fx - Dxt
+# Dy ve Dx hesapla fonksiyonunda elde edilen  deltay ve delta x i temsil eder  
+# Yb ve Xb verilen ilk noktanın koordinatları
+# Yc ve Xc verilen son noktanın koordinatları
+# Bt kırılma açıları
+def kenKapHata(Dy, Dx, Yb, Xb, Yc, Xc, Bt):
+    Dyt = sum(Dy) # Dyt delta y lerin toplamı
+    Dxt = sum(Dx) # Dxt delta x lerin toplamı
+    S = m.sqrt(Dyt**2 + Dxt**2) 
+    Fy = Yc - Yb # y için koordinatlar farkı
+    Fx = Xc - Xb # x için koordinatlar farkı
+    fy = Fy - Dyt # kenar kapatma hatası
+    fx = Fx - Dxt # kenar kapatma hatası
 
-    # fs = m.sqrt(fy**2 + Fx**2) 
-    # kapalı Kapalı poligon hesabında geçki başlanan noktada 
-    # bittiği için enine ve boyuna hataları hesaplanamaz. Bu nedenle doğrusal kapanma hatası 
-    # fs kenar kapanma hatası hesaplanır. 
 
-    fQ = (1/S) * (fy * Dxt - fx * Dyt) 
-    fL = (1/S) * (fy * Dyt - fx * Dxt)
+    fQ = (1/S) * (fy * Dxt - fx * Dyt) # enine kapanma hatası
+    fL = (1/S) * (fy * Dyt - fx * Dxt) # boyuna kapanma hatası
     fQ_max = 0.05 + 0.15 * m.sqrt(S/1000) # S yi kilometre cinsine çevirmemiz gerekiyor bu nedenle 1000 e böldük
     fL_max = 0.05 + 0.04 * m.sqrt(len(Bt)-1)
-    Slis.sort()
-    lenfy = fy*100
-    if lenfy < 0:
-        lenfy * -1
-    lenfx = fx*100
-    if lenfx < 0:
-        lenfx *-1
+    # fqmax ve flmax yönetmelikteki kabul edilebilirliği kontrol ediyor
 
-
+    # buradan sonrası dhunt yöntemi ile orantılı bir şekilde hataları Dy(delta y) ve Dx(delta x) üzerine eklenecek not sayı 0.08 gibi bir değer geliyor ona dikkat etmek gerek hata olabilir
+    # ya da kırılma acısı hatasında olduğu gibi de yapılabilir 
     if fQ < fQ_max and fL < fL_max:
         # deneme.py dosyası içerisindeki yöntem (dhunt) ile 
         # Dy ve Dx üzerine orantılı olarak eklenecek 
         # (dhunt yöntemi orantılı yapıyor)
         return 0
 
+
+# açık poligon ile aynı  SA = (Bt +Xy) + Z bu SA formülünde parantez içerisindeki alan T yi temsil ediyor ve +Z işlemi Kontrol(T) içerisinde yapılıyor
 def kontrol(T):
     if 0<=T and T<=200:
         T+=200
@@ -76,33 +78,33 @@ def kontrol(T):
 
 
 def hesapla(Xy, Bt, Yn, Xn, Sn, IS, SS): 
-    SA_list = []
     x = 0
     i=0
     c = 0
-    Dyl = []
-    Dxl = []
+    Dyl = [] # bulunacak delta y ve delta xler bu iki liste içerisine daha sonra 
+    Dxl = [] # kenar kapatma hesabı ile güncellenmek üzere eklenecek
     kirilmaAcisiHatasi(Bt, IS, SS, devam)
     for i in range (len(yeniBt)):
         # 1.adım
-        T = (Xy+yeniBt[c])
-        SA = kontrol(T)
+        T = (Xy+yeniBt[c]) # konterol(T) fonksiyonunda +z işlemi yapılmak üzere ayrı hesaplanıyor
+        SA = kontrol(T) # ve SA bu işlemden sonra bulunuyor
         # 2.adım 
         Dy = Sn[c]*m.sin(m.radians(SA*(180/200)))
         Dx = Sn[c]*m.cos(m.radians(SA*(180/200)))
         #delta y ve delta x leri listelere aldık daha sonra bu listeler ile kenar kapatma hata hesabı yapılacak
+        # burada güncellenen 
         Dyl.append(Dy)
-        Dxl.append(Dy)
+        Dxl.append(Dx)
         Py = Dy + Yn
         Px = Dx + Xn
         Xy = SA
 
-        n = []
+        n = [] # bu liste sadece bir noktanın koordinatlarını alacak
         n.append(Py)
         n.append(Px)
-        Pn.append(n)
-        Yn = Py
-        Xn =Px       
+        Pn.append(n) # bulunan koordinatlar listesi(n) Pn(sonuç) listesine eklenecek
+        Yn = Py # bir sonraki döngüde başka bir noktaya geçilebilmesi için noktanın y ve x değerleri güncelleniyor
+        Xn = Px       
         c+=1
     
     print(Pn)
